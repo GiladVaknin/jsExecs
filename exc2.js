aa86d7d03987c9401813b18ad2f2f31a0d146e16
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-01-18 13:26:04 +0200

    Exercise 2

---
+++ exc2.js
@@ -0,0 +1,7 @@
+let x = Number(prompt("Enter the first number"));
+let y = Number(prompt("Enter the second number"));
+let z = Number(prompt("Enter the third number"));
+let p = x * y * z;
+if (p > 0) {
+  alert("The sign is +");
+} else alert("The sign is -");
