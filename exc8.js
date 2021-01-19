14a60bd432f6bd9f09a2944a598a0001db2628f2
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-01-19 11:38:56 +0200

    Update exc8.js

--- exc8.js
+++ exc8.js
@@ -1,23 +1,20 @@
-const number = 19;
-// let temp = number;
-// let sum = 0;
-// while (temp > 0) {
-//   const digit = temp % 10;
-//   sum += digit ** 2;
-//   temp = Math.floor(temp / 10);
-// }
-// console.log(sum);
-// /
-let temp = number;
-
-while (temp != 1) {
-  let sum = 0;
-  console.log(temp);
-  while (temp > 0) {
-    const digit = temp % 10;
-    sum += digit ** 2;
-    temp = Math.floor(temp / 10);
+let count = 0;
+for (let i = 0; count < 5; i++) {
+  const number = i;
+  let temp = number;
+  const history = [];
+  while (temp != 1 && !history.includes(temp)) {
+    history.push(temp);
+    let sum = 0;
+    while (temp > 0) {
+      const digit = temp % 10;
+      sum += digit ** 2;
+      temp = Math.floor(temp / 10);
+    }
+    temp = sum;
   }
-  temp = sum;
+  if (temp === 1) {
+    count++;
+    console.log(number + " number is happy number");
+  }
 }
-console.log("number is happy number");
